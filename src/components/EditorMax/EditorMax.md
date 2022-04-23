6dcf37b5a5ebc4026539c3dd768b5ea1d8eca22d
Pavel <braurs64@gmail.com>
2022-04-23 10:16:52 +0400

    rem 2 folders

--- src/components/EditorMax/EditorMax.md
+++
@@ -1,9 +0,0 @@
-### Default example
-
-```jsx
-import React from 'react';
-
-import { EditorMax } from '@texttree/notepad-rcl';
-
-<EditorMax id="some_new_id2" />;
-```
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,1 @@
 export { default as Editor } from './Editor';
-export { default as EditorMax } from './EditorMax';
-export { default as ToolsInstallation } from './ToolsInstallation';
