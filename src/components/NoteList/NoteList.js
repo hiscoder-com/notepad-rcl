fdb425b96a9c3b0598b5e2454d3836e55c6ff6a4
Pavel <braurs64@gmail.com>
2022-05-30 15:15:02 +0400

    loading data from localforage

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -1,95 +1,35 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react';
-import localForage from 'localforage';
-import { v4 as uuidv4 } from 'uuid';
+import localforage from 'localforage';
 
-function NoteList() {
-  const [notesArray, setNotesArray] = useState(
-    localForage.notesArray ? JSON.parse(localForage.notesArray) : []
-  );
-  const [activeNoteId, setActiveNoteId] = useState(false);
+function NoteList({}) {
+  const [state, setState] = useState(0);
 
   useEffect(() => {
-    localForage.setItem('notesArray', notesArray);
-  }, [notesArray]);
+    localforage.setItem('test', state).then(() => {
+      console.log('used localForage');
+    });
+  }, [state]);
+  useEffect(() => {
+    localforage.getItem('test').then((val) => {
+      if (val !== null) {
+        setState(val);
+      } else {
+        localforage.setItem('test', state);
+      }
+    });
+  }, []);
 
-  const addNote = () => {
-    const newNote = {
-      id: uuidv4(),
-      title: 'Untitled Note',
-      body: '',
-      lastModified: Date.now(),
-    };
-
-    setNotesArray([newNote, ...notesArray]);
-  };
-
-  const deleteNote = (noteId) => {
-    setNotesArray(notesArray.filter(({ id }) => id !== noteId));
-  };
   return (
-    <div className="app-sidebar">
-      <div
-        className="app-sidebar-header"
-        style={{ display: 'flex', justifyContent: 'space-between', padding: '25px' }}
-      >
-        <h1>Notes</h1>
-        <button
-          onClick={addNote}
-          style={{
-            color: '#08c',
-          }}
-        >
-          Add
-        </button>
-      </div>
-      <div
-        className="app-sidebar-notes"
-        style={{
-          height: 'calc(100vh - 78px)',
-          overflowY: 'scroll',
+    <>
+      <p>{state}</p>
+      <button
+        onClick={() => {
+          setState(Math.floor(Math.random() * 10));
         }}
       >
-        {notesArray.map(({ id, title, lastModified }, i) => (
-          <div
-            className={`app-sidebar-note ${id === activeNoteId && 'active'}`}
-            key={i}
-            onClick={() => setActiveNoteId(id)}
-            style={{
-              padding: '25px',
-              cursor: 'pointer',
-            }}
-          >
-            <div
-              className="sidebar-note-title"
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-              }}
-            >
-              <strong>{title}</strong>
-              <button
-                onClick={() => deleteNote(id)}
-                style={{
-                  color: 'red',
-                }}
-              >
-                Delete
-              </button>
-            </div>
-            <small className="note-meta">
-              Last modified{' '}
-              {new Date(lastModified).toLocaleDateString('ru-RU', {
-                hour: '2-digit',
-                minute: '2-digit',
-              })}
-            </small>
-          </div>
-        ))}
-      </div>
-    </div>
+        click to get random number
+      </button>
+    </>
   );
 }
 export default NoteList;
