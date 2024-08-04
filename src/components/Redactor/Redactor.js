dc570847dbebc9c04815b1d80a8661f93898b49c
valyukhov <alexzed@bk.ru>
2024-08-04 17:22:04 +0300

    added auto rtl direction

--- src/components/Redactor/Redactor.js
+++ src/components/Redactor/Redactor.js
@@ -1,7 +1,5 @@
 import { default as React, useState, useEffect, useRef } from 'react';
-
 import PropTypes from 'prop-types';
-
 import { createReactEditorJS } from 'react-editor-js';
 
 function Redactor({
@@ -11,26 +9,56 @@ function Redactor({
   setActiveNote,
   editorTools,
   classes = {},
-  isRtl = false,
+  handleRtl = false,
   readOnly = false,
   disableTitle = false,
   isSelectableTitle = false,
-  emptyTitle = 'Empty Title',
+  emptyTitle = 'Empty title',
 }) {
   const titleRef = useRef(null);
-
   const ReactEditorJS = createReactEditorJS();
 
   const [title, setTitle] = useState(activeNote?.title || emptyTitle);
+  const [titleDirection, setTitleDirection] = useState(
+    calculateRtlDirection(activeNote?.title || emptyTitle)
+  );
+  const [redactorDirection, setRedactorDirection] = useState(
+    calculateRtlDirection(
+      activeNote?.data
+        ? activeNote.data.blocks.map((block) => block.data.text).join(' ')
+        : ''
+    )
+  );
 
   useEffect(() => {
     if (activeNote?.title) {
       setTitle(activeNote.title);
+      updateDirection(activeNote.title, setTitleDirection);
     }
   }, [activeNote]);
 
+  function filterText(text) {
+    return text.replace(/<br>/g, '').replace(/\s|&nbsp;/g, '');
+  }
+
+  function calculateRtlDirection(text) {
+    const filteredText = filterText(text);
+    const totalChars = filteredText.length;
+    const rtlChars = (
+      filteredText.match(/[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/g) || []
+    ).length;
+    return (totalChars > 0 && rtlChars / totalChars > 0.5) || handleRtl ? 'rtl' : 'ltr';
+  }
+
+  const updateDirection = (text, setDirection) => {
+    const direction = calculateRtlDirection(text);
+    setDirection(direction);
+  };
+
   const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+    updateDirection(newTitle, setTitleDirection);
   };
 
   const handleTitleBlur = () => {
@@ -39,10 +67,13 @@ function Redactor({
       setActiveNote((prev) => ({ ...prev, title: updatedTitle }));
     }
     setTitle(updatedTitle);
+    updateDirection(updatedTitle, setTitleDirection);
   };
 
   const handleEditorChange = async (e) => {
     const content = await e.saver.save();
+    const combinedText = content.blocks.map((block) => block.data.text).join(' ');
+    updateDirection(combinedText, setRedactorDirection);
     if (typeof setActiveNote === 'function') {
       setActiveNote((prev) => ({ ...prev, data: content }));
     }
@@ -55,7 +86,6 @@ function Redactor({
     selection.removeAllRanges();
     selection.addRange(range);
   };
-  const direction = isRtl ? 'rtl' : 'ltr';
 
   return (
     <div className={classes.wrapper}>
@@ -71,10 +101,10 @@ function Redactor({
           onBlur={handleTitleBlur}
           onClick={() => isSelectableTitle && selectTitle()}
           ref={titleRef}
-          dir={direction}
+          dir={titleDirection}
         />
       )}
-      <div className={classes.redactor} dir={direction}>
+      <div className={classes.redactor} dir={redactorDirection}>
         <ReactEditorJS
           onChange={handleEditorChange}
           autofocus={false}
@@ -93,32 +123,19 @@ function Redactor({
 
 Redactor.propTypes = {
   classes: PropTypes.shape({
-    /** Class for wrapper */
     wrapper: PropTypes.string,
-    /** Class for redactor */
     redactor: PropTypes.string,
-    /** Class for title */
     title: PropTypes.string,
   }),
-  /** Write a new property for the Tools object and pass it to the Editor via the addTools variable */
   editorTools: PropTypes.object,
-  /** Note ID. To run Redactor, you need to assign an ID to the note */
   initId: PropTypes.string,
-  /** Note Placeholder */
   placeholder: PropTypes.string,
-  /** An object that contains all the properties of the note */
   activeNote: PropTypes.object,
-  /** Pass a new note object to the setter */
   setActiveNote: PropTypes.func,
-  /** If true, then the content is read-only */
   readOnly: PropTypes.bool,
-  /** If true, then the title is not displayed */
   disableTitle: PropTypes.bool,
-  /** Sets the title value if the title is empty */
   emptyTitle: PropTypes.string,
-  /** If true, then the title selected by click */
   isSelectableTitle: PropTypes.bool,
-  /** if true, display note, title in rtl direction */
   isRtl: PropTypes.bool,
 };
 
---
+++ src/components/Redactor/useRtlDirection.md
@@ -0,0 +1,157 @@
+### With this hook, we save the note to the database using localforage
+
+```jsx
+import { useState, useEffect } from 'react';
+
+import { ListOfNotes, useData, Redactor } from '@texttree/notepad-rcl';
+
+function Component() {
+  const [noteId, setNoteId] = useState('test_noteId');
+  const [activeNote, setActiveNote] = useState(null);
+
+  useEffect(() => {
+    const getNote = async (id) => {
+      const result = await noteRequest(id);
+      setActiveNote(result);
+      return result;
+    };
+    getNote(noteId);
+  }, [noteId]);
+
+  return (
+    <div>
+      <div>
+        {!activeNote ? (
+          <>
+            <ListOfNotes
+              notes={notes}
+              removeNote={removeNote}
+              setNoteId={setNoteId}
+              classes={{
+                wrapper: '',
+                item: ' m-5 p-5 bg-yellow-200 h-31 hover:bg-yellow-100 cursor-pointer rounded-lg shadow-md',
+                title: 'mr-10 font-bold',
+                text: 'overflow-hidden',
+                delBtn: 'bg-orange-300 p-2 mt-4 rounded-lg',
+              }}
+              delBtnName={'Delete'}
+              isShowDate
+              dateOptions={{
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+              }}
+            />
+            <div className="flex justify-end">
+              <button
+                className="text-3xl bg-gray-400 px-4 py-1 rounded-xl hover:bg-orange-300"
+                onClick={() =>
+                  addNote('My custom note title', 'Custom text for new note...')
+                }
+              >
+                +
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className={'bg-yellow-200 p-6 relative shadow-md'}>
+            <Redactor
+              activeNote={activeNote}
+              setActiveNote={setActiveNote}
+              initId={'second'}
+              classes={{ title: 'bg-inherit font-bold' }}
+            />
+            <button
+              className={'bg-orange-300 px-4 py-2  rounded-lg '}
+              onClick={() => saveNote(noteId, activeNote)}
+            >
+              save
+            </button>
+            <button
+              className={
+                'bg-orange-300 px-4 py-2 text-lg rounded-lg absolute right-3 top-3'
+              }
+              onClick={() => {
+                setActiveNote(null);
+                setNoteId(null);
+              }}
+            >
+              x
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+<Component />;
+```
+
+<!--
+### If you need to set the database name, you can use the dBNameRegistration method
+
+```jsx
+import React from 'react';
+import { useData, Redactor } from '@texttree/notepad-rcl';
+
+const { dBNameRegistration, getNote, saveNote } = useData();
+
+dBNameRegistration('NotepadRCL');
+
+<Redactor getNote={getNote} saveNote={saveNote} initId={'second'} />;
+``` -->
+
+<!--
+### You can also get and edit data from the database using the useState hook.
+
+```jsx
+import { useState, useEffect } from 'react';
+import { ListOfNotes, useData, Redactor } from '@texttree/notepad-rcl';
+
+function Component() {
+  const { notes, removeNote, addNote, dBNameRegistration, getNote, saveNote } = useData();
+
+  const [noteId, setNoteId] = useState('');
+
+  const changePlaceholder = 'changed default text';
+
+  const inputStyle = {
+    width: '650px',
+    height: '38px',
+    fontSize: 'large',
+    border: 'none',
+    outline: 'none',
+  };
+
+  dBNameRegistration('NotepadRCL');
+
+  return (
+    <div style={{ display: 'inline-flex' }}>
+      <div style={{ width: '50%' }}>
+        <ListOfNotes
+          notes={notes}
+          listName="List of Notes"
+          removeNote={removeNote}
+          addNote={addNote}
+          noteId={setNoteId}
+        />
+      </div>
+      <div style={{ width: '50%' }}>
+        <Redactor
+          initId={'third'}
+          placeholder={changePlaceholder}
+          getNote={getNote}
+          saveNote={saveNote}
+        />
+      </div>
+    </div>
+  );
+}
+
+<Component />;
+``` -->
